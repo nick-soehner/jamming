2b5733c8ed5b86182f45525a9a78d4fef74eac61
Nick <soehner.nick@gmail.com>
2021-10-18 12:16:33 -0600

    added more components

---
+++ src/Components/Tracklist/Tracklist.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Tracklist.css';
+
+class Tracklist extends React.Component {
+    render() {
+        return(
+            <div className="TrackList">
+            </div>
+        )
+    }
+}
+
+export default Tracklist;
