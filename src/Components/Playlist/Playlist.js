c33ecac16166db33cd10b7f9465b2c2985bbb6bb
Nick <soehner.nick@gmail.com>
2021-10-19 12:15:29 -0600

    added fucntionablity to track adding

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,12 +1,13 @@
 import React from "react";
 import './Playlist.css';
+import Tracklist from "../Tracklist/Tracklist";
 
 class Playlist extends React.Component {
     render() {
         return (
             <div className="Playlist">
                 <input deafultValue={"New Playlist"}/>
-                {/* <Tracklist /> */}
+                <Tracklist tracks={this.props.playlistTracks} />
                 <button className="Playlist-save">SAVE TO SPOTIFY</button>
             </div>
         )
--- src/Components/Tracklist/Tracklist.js
+++ src/Components/Tracklist/Tracklist.js
@@ -1,10 +1,16 @@
 import React from "react";
 import './Tracklist.css';
+import Track from '../Track/Track';
 
 class Tracklist extends React.Component {
     render() {
         return(
             <div className="TrackList">
+                {
+                    this.props.tracks.map(track => {
+                        return <Track  track={track} key={track.id} onAdd={this.props.onAdd} />
+                    })
+                }
             </div>
         )
     }
